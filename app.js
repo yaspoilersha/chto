97120b6706795d6cb260ab4eaebe167cf0acb622
yaspoilersha <spoilershaprod@gmail.com>
2022-10-16 15:25:59 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll ('section');
+const sectBtns = document.querySelectorAll ('controlls');
+const sectBtn = document.querySelectorAll ('control');
+const allSections = document.querySelector ('main-content');
+
+
+function PageTransitions(){
+    for (let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll ('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
+            this.className += ' active-btn';
+        })
+    }
+}
+
+PageTransitions();
